25d4ea313b6221359d1e148ed687c17cf57f081e
PA1393 <pouya.anvari1393@gmail.com>
2026-02-19 17:19:02 -0800

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@application-tracking-system/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
