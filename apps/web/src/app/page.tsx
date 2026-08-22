1bc83b803d48800f573d07618525e444f2772716
Pouya Anvari <pouya.anvari1393@gmail.com>
2026-08-22 10:13:54 -0700

    Redirect / to /login

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,16 +1,5 @@
-"use client";
-
-
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="container mx-auto max-w-3xl px-4 py-2">
- 
-      {/* <div className="grid gap-6">
-        <section className="rounded-lg border p-4">
-       
-        </section>
-      </div> */}
-    </div>
-  );
+  redirect("/login");
 }
